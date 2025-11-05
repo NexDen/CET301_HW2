2636a923a33a3750ec7761b9ed60b9c9f4707886
Hüseyin Şimşek <Huseyin.Simsek@obase.com>
2025-11-05 15:27:47 +0300

    commet added

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -15,6 +15,8 @@ public partial class MainPage : ContentPage
     private void OnNumberPressed(object? sender, EventArgs e)
     {
         Button pressedButton = sender as Button;
+        //test açıklama
+        //test açıklama
         if (pressedButton != null)
         {
             if (isFirstNumberAfterOperator)
